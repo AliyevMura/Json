1fcc186c0859d418a6775a7fda92a9435ce0e76c
AliyevMura <7lvghl9@code.edu.az>
2023-05-02 11:30:02 +0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿#region Task1
+
+using Newtonsoft.Json;
+
+//string Path = "C:\\Users\\lenovo\\source\\repos\\Json\\Json\\Models\\test.cs";
+//Directory.CreateDirectory(Path);
+//File.Create(Path);
+//string DataPath = "C:\\Users\\lenovo\\source\\repos\\Json\\Json\\Data\\jjson.json";
+//Directory.CreateDirectory(DataPath);
+//File.Create(DataPath);
+
+
+#endregion
+
+
+//Task2
+var url = @"https://jsonplaceholder.typicode.com/posts";
+
+HttpClient client = new HttpClient();
+var poster = await client.GetStringAsync(url);
+
+
+//Console.WriteLine(JsonConvert.SerializeObject(poster));
+
+
+using(StreamWriter sw = new StreamWriter(poster))
+{
+    sw.WriteLine(JsonConvert.SerializeObject(poster));
+}
